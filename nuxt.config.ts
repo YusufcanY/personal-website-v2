5da0d5972d016a25826fd1859658d479c372f8f3
Yusufcan Yılmaz <playders03@gmail.com>
2022-06-17 11:55:52 +0300

    Meta config added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'Yusufcan Yılmaz',
+      title: 'Yusufcan Yılmaz | Frontend Developer',
       link: [
         {
           rel: 'preconnect',
@@ -25,6 +25,63 @@ export default defineNuxtConfig({
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;200;300;400;500;600;700&display=swap',
+        },
+      ],
+      meta: [
+        {
+          name: 'description',
+          content:
+            'Yusufcan Yılmaz is a frontend developer who is passionate about building beautiful and functional websites.',
+        },
+        {
+          name: 'theme-color',
+          content: '#0057ff',
+        },
+        {
+          property: 'og:url',
+          content: 'https://yusufcanyilmaz.com/',
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          property: 'og:title',
+          content: 'Yusufcan Yılmaz | Frontend Developer',
+        },
+        {
+          property: 'og:description',
+          content:
+            'Yusufcan Yılmaz is a frontend developer who is passionate about building beautiful and functional websites.',
+        },
+        {
+          property: 'og:image',
+          content: '/img/yusufcan.jpeg',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          property: 'twitter:domain',
+          content: 'yusufcanyilmaz.com',
+        },
+        {
+          property: 'twitter:url',
+          content: 'https://yusufcanyilmaz.com/',
+        },
+        {
+          name: 'twitter:title',
+          content: 'Yusufcan Yılmaz | Frontend Developer',
+        },
+        {
+          name: 'twitter:description',
+          content:
+            'Yusufcan Yılmaz is a frontend developer who is passionate about building beautiful and functional websites.',
+        },
+        {
+          name: 'twitter:image',
+          content: '/img/yusufcan.jpeg',
         },
       ],
     },
