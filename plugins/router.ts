fa16fbdaf17610621397ec88122a137554fce0f5
Yusufcan Yılmaz <playders03@gmail.com>
2022-07-04 13:22:03 +0300

    Unnecessary console.log deleted from router.ts

--- plugins/router.ts
+++ plugins/router.ts
@@ -2,7 +2,6 @@ import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('page:finish', async (a) => {
-    console.log('s')
     window.scrollTo({ top: 0 })
   })
 })
