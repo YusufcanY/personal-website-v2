6e721b464f4ffe8c9e79c5c651a0d15909063768
Yusufcan Yılmaz <55593600+YusufcanY@users.noreply.github.com>
2022-10-06 12:53:11 +0300

    Update meta title and description

--- app.vue
+++ app.vue
@@ -2,13 +2,13 @@
   const route = useRoute()
   const title = computed(() => {
     return route.meta.title
-      ? route.meta.title + ' | Yusufcan Yılmaz'
-      : 'Yusufcan Yılmaz | Frontend Developer'
+      ? route.meta.title + ' | Yusufcan Yılmaz - Frontend Developer'
+      : 'Yusufcan Yılmaz - Frontend Developer'
   })
   const description = computed(() => {
     return route.meta.description
       ? route.meta.description
-      : 'Yusufcan Yılmaz is a Frontend Developer. Love skating and coding.'
+      : 'Yusufcan Yılmaz is a Frontend Developer. Working on Vue.js, Nuxt.js and TailwindCSS.'
   })
   useHead({
     title,
