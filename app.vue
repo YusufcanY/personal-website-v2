31e0790f22d146e8b205acd8969a4d02a2fbaed1
YusufcanY <playders03@gmail.com>
2023-05-04 11:59:00 +0300

    Added "Offline Ready" toast

--- app.vue
+++ app.vue
@@ -28,8 +28,26 @@
   })
 </script>
 <template>
-  <div>
+  <div class="relative">
     <VitePwaManifest />
     <NuxtPage />
+    <Transition name="fade">
+      <div
+        v-if="$pwa.offlineReady"
+        class="bg-dark-foreground/5 dark:bg-light-foreground/5 text-dark-foreground dark:text-light-foreground fixed bottom-8 left-1/2 hidden -translate-x-1/2 rounded-full px-8 py-2 text-sm lg:block"
+      >
+        Offline Ready
+      </div>
+    </Transition>
   </div>
 </template>
+<style scoped>
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.2s;
+  }
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
